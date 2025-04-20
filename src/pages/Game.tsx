8a25bb471297ef9a3da535263915681120342dec
md-sazzadul <md.sazzadul.islam15@gmail.com>
2025-04-20 13:12:16 +0600

    add dice component in the game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,15 +1,22 @@
+import Dice from "@/components/Dice";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 const Game = () => {
   const [diceValue, setDiceValue] = useState<number | null>(null);
 
+  const handleDiceRoll = (value: number) => {
+    setDiceValue(value);
+  };
+
   return (
     <div>
       <h1>🎲 LudoCraft 🎲</h1>
       <p>Rolled: {diceValue ?? "-"}</p>
-      {/* TO DO: Dice Component */}
+      <Dice onRoll={handleDiceRoll} />
       <Button>Start Game</Button>
     </div>
   );
 };
+
+export default Game;
